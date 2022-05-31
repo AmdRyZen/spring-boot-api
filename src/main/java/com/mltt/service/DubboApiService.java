1799380a9aa5ab557eac3f5b80a8b3dd14885cd9
Tianshizhiwang·Yan <tianshizhiwangyan@gmail.com>
2022-05-31 16:52:44 +0800

    update Triple && Stream

--- src/main/java/com/mltt/service/DubboApiService.java
+++ src/main/java/com/mltt/service/DubboApiService.java
@@ -1,9 +1,14 @@
 package com.mltt.service;
 
 import com.mltt.biz.model.FUser;
+import org.apache.dubbo.common.stream.StreamObserver;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface DubboApiService {
     FUser getFuserList();
+
+    StreamObserver<String> sayHelloStream(StreamObserver<String> response);
+
+    void sayHelloServerStream(String request, StreamObserver<String> response);
 }
--- src/main/java/com/mltt/service/impl/DubboApiServiceImpl.java
+++ src/main/java/com/mltt/service/impl/DubboApiServiceImpl.java
@@ -1,8 +1,10 @@
 package com.mltt.service.impl;
 
 import com.mltt.biz.model.FUser;
+import com.mltt.exception.ServiceException;
 import com.mltt.mapper.FUserMapper;
 import com.mltt.service.DubboApiService;
+import org.apache.dubbo.common.stream.StreamObserver;
 import org.apache.dubbo.config.annotation.DubboService;
 
 import javax.annotation.Resource;
@@ -15,6 +17,36 @@ public class DubboApiServiceImpl implements DubboApiService {
     public FUser getFuserList() {
         FUser fUserVo = fUserMapper.selectById(1L);
         return fUserVo;
+    }
+
+    @Override
+    public StreamObserver<String> sayHelloStream(StreamObserver<String> response) {
+       return new StreamObserver<String>() {
+           @Override
+           public void onNext(String data) {
+               System.out.println(data);
+               response.onNext("hello,"+data);
+           }
+
+           @Override
+           public void onError(Throwable throwable) {
+               throw new ServiceException(400, throwable.getMessage());
+           }
+
+           @Override
+           public void onCompleted() {
+               System.out.println("onCompleted");
+               response.onCompleted();
+           }
+       };
+    }
+
+    @Override
+    public void sayHelloServerStream(String request, StreamObserver<String> response) {
+        for (int i = 0; i < 10; i++) {
+            response.onNext("hello," + request);
+        }
+        response.onCompleted();
     }
 
 }
